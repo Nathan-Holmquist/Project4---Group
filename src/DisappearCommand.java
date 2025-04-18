c44d5267b495e0a96fd2e2c55dff7e4c122635fe
Nathan Holmquist <nholmquist2005@aol.com>
2025-04-18 19:49:11 -0400

    DisappearCommand Done

--- src/DisappearCommand.java
+++ src/DisappearCommand.java
@@ -1,9 +1,29 @@
 public class DisappearCommand implements UserInputCommand {
 
-    @Override
-    public String carryOut() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'carryOut'");
+    Item item;
+    TourStatus tourStatus;
+    String message;
+
+    public DisappearCommand(String itemName) {
+        this.tourStatus = TourStatus.getInstance();
+        this.item = tourStatus.getItemFromBackpack(itemName);
+
+        
+        System.out.println(carryOut());
+
     }
     
+
+    @Override
+    public String carryOut() {
+
+        if (this.item != null) {
+            tourStatus.removeItemFromBackpack(item);
+            message = item.getName() + " has disappeared from your backpack.";
+        } else {
+            message = "Item not found in backpack.";
+        }
+
+        return message;
+    }
 }
